aff8c1b734715db5edef8f986360f6cb19d5c413
jreese <jonas.reese@direkt-gruppe.de>
2014-09-22 18:57:34 +0200

    Fixes & ongoing development Spring MVC

--- nexuse2e-core/src/main/java/org/nexuse2e/ui/controller/service/ServiceController.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/ui/controller/service/ServiceController.java
@@ -1,16 +1,46 @@
+/**
+ *  NEXUSe2e Business Messaging Open Source
+ *  Copyright 2000-2009, Tamgroup and X-ioma GmbH
+ *
+ *  This is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation version 2.1 of
+ *  the License.
+ *
+ *  This software is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this software; if not, write to the Free
+ *  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package org.nexuse2e.ui.controller.service;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
 
+import javax.validation.Valid;
+
+import org.nexuse2e.BeanStatus;
+import org.nexuse2e.NexusException;
+import org.nexuse2e.configuration.ConfigurationUtil;
 import org.nexuse2e.configuration.EngineConfiguration;
 import org.nexuse2e.configuration.GenericComparator;
+import org.nexuse2e.pojo.ComponentPojo;
+import org.nexuse2e.pojo.ServiceParamPojo;
 import org.nexuse2e.pojo.ServicePojo;
+import org.nexuse2e.service.Service;
 import org.nexuse2e.ui.form.ServiceForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Controller for service maintenance.
@@ -25,17 +55,158 @@ public class ServiceController {
 
         List<ServicePojo> services = engineConfiguration.getServices();
         List<ServiceForm> serviceList = new ArrayList<ServiceForm>();
-        TreeSet<ServicePojo> sortedServices = new TreeSet<ServicePojo>( new GenericComparator<ServicePojo>( "name", true ) );
-        sortedServices.addAll( services );
-        for ( ServicePojo service : sortedServices ) {
+        TreeSet<ServicePojo> sortedServices = new TreeSet<ServicePojo>(new GenericComparator<ServicePojo>("name", true));
+        sortedServices.addAll(services);
+        for (ServicePojo service : sortedServices) {
             ServiceForm serviceForm = new ServiceForm();
-            serviceForm.setProperties( service );
-            serviceForm.setServiceInstance( engineConfiguration.getService(
-                    service.getName() ) );
+            serviceForm.setProperties(service);
+            serviceForm.setServiceInstance(engineConfiguration.getService(service.getName()));
             serviceList.add( serviceForm );
         }
         model.addAttribute("collection", serviceList);
 
         return "pages/services/service_list";
+    }
+    
+    @RequestMapping("/ServiceView.do")
+    public String serviceView(ServiceForm serviceForm, BindingResult bindingResult, Model model, EngineConfiguration engineConfiguration)
+            throws NexusException {
+        
+        ServicePojo servicePojo = engineConfiguration.getServicePojoByNxServiceId(serviceForm.getNxServiceId());
+        serviceForm.setProperties(servicePojo);
+
+        if (servicePojo != null) {
+            try {
+                Service service = engineConfiguration.getServiceInstanceFromPojo(servicePojo);
+                for (ServiceParamPojo serviceParam : servicePojo.getServiceParams()) {
+                    serviceParam.setParameterDescriptor(service.getParameterMap().get(serviceParam.getParamName()));
+                }
+                serviceForm.setParameters(ConfigurationUtil.getConfiguration(service, servicePojo));
+                serviceForm.createParameterMapFromPojos();
+                serviceForm.setServiceInstance(service);
+            } catch (NexusException ex) {
+                if (ex.getCause() instanceof ClassNotFoundException) {
+                    String className = servicePojo.getComponent() != null ? servicePojo.getComponent().getClassName() : "<unknown>";
+                    bindingResult.reject("error.component.classnotfound", new Object[] { className }, "Component class " + className + " not found");
+                } else {
+                    throw ex;
+                }
+            }
+            
+            List<ServicePojo> services = engineConfiguration.getServices();
+            List<ServicePojo> sortedServices = new ArrayList<ServicePojo>(services.size());
+            sortedServices.addAll(engineConfiguration.getServices());
+            Collections.sort(sortedServices, new GenericComparator<ServicePojo>("name", true));
+            Service serviceInstance = engineConfiguration.getService(servicePojo.getName());
+            model.addAttribute("serviceStatus", serviceInstance == null ? BeanStatus.UNDEFINED : serviceInstance.getStatus());
+            model.addAttribute("services", sortedServices);
+        }
+        
+        return "pages/services/service_view";
+    }
+
+    @RequestMapping("/ServiceStart.do")
+    public String serviceStart(
+            Model model,
+            ServiceForm serviceForm,
+            BindingResult bindingResult,
+            @RequestParam(value = "viewService", defaultValue = "false") boolean viewService,
+            EngineConfiguration engineConfiguration)
+                    throws NexusException {
+
+        ServicePojo servicePojo = engineConfiguration.getServicePojoByNxServiceId(serviceForm.getNxServiceId());
+        if (servicePojo != null) {
+            Service service = engineConfiguration.getService(servicePojo.getName());
+            if (service != null && service.getStatus() == BeanStatus.ACTIVATED) {
+                service.start();
+            }
+        }
+
+        if (viewService) {
+            return serviceView(serviceForm, bindingResult, model, engineConfiguration);
+        }
+        return serviceList(model, engineConfiguration);
+    }
+
+    @RequestMapping("/ServiceStop.do")
+    public String serviceStop(
+            Model model,
+            @Valid ServiceForm serviceForm,
+            BindingResult bindingResult,
+            @RequestParam(value = "viewService", defaultValue = "false") boolean viewService,
+            EngineConfiguration engineConfiguration)
+                    throws NexusException {
+
+        ServicePojo servicePojo = engineConfiguration.getServicePojoByNxServiceId(serviceForm.getNxServiceId());
+        if (servicePojo != null) {
+            Service service = engineConfiguration.getService(servicePojo.getName());
+            if (service != null && service.getStatus() == BeanStatus.STARTED) {
+                service.stop();
+            }
+        }
+
+        if (viewService) {
+            return serviceView(serviceForm, bindingResult, model, engineConfiguration);
+        }
+        return serviceList(model, engineConfiguration);
+    }
+
+    @RequestMapping("/ServiceUpdate.do")
+    public String serviceUpdate(ServiceForm serviceForm, BindingResult bindingResult, Model model, EngineConfiguration engineConfiguration)
+            throws NexusException {
+
+        ServicePojo originalService = engineConfiguration.getServicePojoByNxServiceId(serviceForm.getNxServiceId());
+
+        ComponentPojo component = null;
+
+        if (!bindingResult.hasErrors()) {
+            if (originalService != null) {
+                if (originalService != null) {
+                    component = originalService.getComponent();
+                }
+            }
+            if (component == null) {
+                component = engineConfiguration.getComponentByNxComponentId(serviceForm.getNxComponentId());
+            }
+    
+            if (component != null && serviceForm.getSubmitted() != null && serviceForm.getSubmitted().equals("true") ) {
+                serviceForm.setSubmitted("false");
+    
+                if (originalService == null) {
+                    originalService = new ServicePojo();
+                    originalService.setName(serviceForm.getName());
+                    originalService.setComponent(component);
+                }
+    
+                originalService.setAutostart(serviceForm.isAutostart());
+                
+                serviceForm.fillPojosFromParameterMap();
+                List<ServiceParamPojo> list = serviceForm.getParameters();
+                for (ServiceParamPojo param : list) {
+                    param.setService(originalService);
+                }
+    
+                if (!originalService.getName().equals(serviceForm.getName())) {
+                    engineConfiguration.renameService(originalService.getName(), serviceForm.getName());
+                    originalService.setName(serviceForm.getName());
+                }
+                originalService.setServiceParams(list);
+    
+                engineConfiguration.updateService(originalService);
+                ConfigurationUtil.configureService(serviceForm.getServiceInstance(), list);
+            }
+
+            return serviceList(model, engineConfiguration);
+        } else {
+            List<ServicePojo> services = engineConfiguration.getServices();
+            List<ServicePojo> sortedServices = new ArrayList<ServicePojo>(services.size());
+            sortedServices.addAll(engineConfiguration.getServices());
+            Collections.sort(sortedServices, new GenericComparator<ServicePojo>("name", true));
+            Service serviceInstance = (originalService != null ? engineConfiguration.getService(originalService.getName()) : null);
+            model.addAttribute("serviceStatus", serviceInstance == null ? BeanStatus.UNDEFINED : serviceInstance.getStatus());
+            model.addAttribute("services", sortedServices);
+
+            return "pages/services/service_view";
+        }
     }
 }
--- nexuse2e-core/src/main/java/org/nexuse2e/ui/form/ServiceForm.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/ui/form/ServiceForm.java
@@ -19,14 +19,14 @@
  */
 package org.nexuse2e.ui.form;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.validation.constraints.Size;
 
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionMapping;
 import org.nexuse2e.configuration.ParameterDescriptor;
 import org.nexuse2e.configuration.ParameterType;
 import org.nexuse2e.pojo.ServiceParamPojo;
@@ -34,12 +34,11 @@ import org.nexuse2e.pojo.ServicePojo;
 import org.nexuse2e.service.Service;
 
 /**
- * The <code>ActionForm</code> used to retrieve and store information
- * for a <code>Service</code>
+ * The form used to retrieve and store information for a <code>Service</code>
  *
  * @author jonas.reese
  */
-public class ServiceForm extends ActionForm {
+public class ServiceForm implements Serializable {
 
     private static final long       serialVersionUID    = 1L;
 
@@ -53,41 +52,37 @@ public class ServiceForm extends ActionForm {
     private boolean                 autostart           = false;
     
     private String                  submitted           = "false";
-    private HashMap<String, String> logFilterValues     = null;
-    private HashMap<String, String> pipeletParamValues  = new HashMap<String, String>();
+    private Map<String, String>     logFilterValues     = null;
+    private Map<String, String>     pipeletParamValues  = new HashMap<String, String>();
     private List<ServiceParamPojo>  parameters          = new ArrayList<ServiceParamPojo>();
 
     private Service                 serviceInstance;
 
-    /* (non-Javadoc)
-     * @see org.apache.struts.action.ActionForm#reset(org.apache.struts.action.ActionMapping, javax.servlet.http.HttpServletRequest)
-     */
-    @Override
-    public void reset( ActionMapping arg0, HttpServletRequest arg1 ) {
+    public void reset() {
 
         submitted = "false";
         componentName = null;
         nxServiceId = 0;
-        if ( logFilterValues != null ) {
+        if (logFilterValues != null) {
             logFilterValues.clear();
         }
-        if ( pipeletParamValues != null ) {
+        if (pipeletParamValues != null) {
             pipeletParamValues.clear();
         }
         autostart = false;
         
-    } // reset
+    }
 
     /**
      * Set the properties of this form based on a POJO
      * @param notifier The POJO used to fill in the fields
      */
-    public void setProperties( ServicePojo service ) {
+    public void setProperties(ServicePojo service) {
 
         nxServiceId = service.getNxServiceId();
         nxComponentId = service.getComponent() == null ? 0 : service.getComponent().getNxComponentId();
         name = service.getName();
-        if ( service.getComponent() != null ) {
+        if (service.getComponent() != null) {
             nxComponentId = service.getComponent().getNxComponentId();
             componentName = service.getComponent().getName();
         }
@@ -98,9 +93,9 @@ public class ServiceForm extends ActionForm {
      * Set the properties of this form based on a POJO
      * @return The updated POJO
      */
-    public ServicePojo getProperties( ServicePojo service ) {
+    public ServicePojo getProperties(ServicePojo service) {
 
-        service.setName( name );
+        service.setName(name);
         return service;
     }
 
@@ -108,225 +103,156 @@ public class ServiceForm extends ActionForm {
 
         pipeletParamValues = new HashMap<String, String>();
         for (ServiceParamPojo param : getParameters()) {
-            pipeletParamValues.put( param.getParamName(), param.getValue() );
+            pipeletParamValues.put(param.getParamName(), param.getValue());
         }
 
     }
 
     public void fillPojosFromParameterMap() {
 
-        if ( pipeletParamValues == null ) {
+        if (pipeletParamValues == null) {
             return;
         }
-        for ( ServiceParamPojo param : getParameters() ) {
-            ParameterDescriptor pd = serviceInstance.getParameterMap().get( param.getParamName() );
+        for (ServiceParamPojo param : getParameters()) {
+            ParameterDescriptor pd = serviceInstance.getParameterMap().get(param.getParamName());
             if (pd != null) {
-                String value = pipeletParamValues.get( param.getParamName() );
+                String value = pipeletParamValues.get(param.getParamName());
                 if (pd.getParameterType() == ParameterType.BOOLEAN) {
-                    if ("on".equalsIgnoreCase( value )) {
+                    if ("on".equalsIgnoreCase(value)) {
                         value = Boolean.TRUE.toString();
                     }
                 }
                 if (value == null) {
                     value = Boolean.FALSE.toString();
                 }
-                param.setValue( value );
+                param.setValue(value);
             }
         }
     }
 
-    /**
-     * @return the pipeletParamValues
-     */
-    public HashMap<String, String> getPipeletParamValues() {
+    public Map<String, String> getPipeletParamValues() {
 
         return pipeletParamValues;
     }
 
-    /**
-     * @param pipeletParamValues the pipeletParamValues to set
-     */
-    public void setPipeletParamValues( HashMap<String, String> pipeletParamValues ) {
+    public void setPipeletParamValues(HashMap<String, String> pipeletParamValues) {
 
         this.pipeletParamValues = pipeletParamValues;
     }
 
-    public Object getParamValue( String key ) {
+    public Object getParamValue(String key) {
 
-        return pipeletParamValues.get( key );
+        return pipeletParamValues.get(key);
     }
 
-    public void setParamValue( String key, Object value ) {
+    public void setParamValue(String key, Object value) {
 
-        // LOG.trace("key: "+key); 
-        // LOG.trace("value: "+value.toString());
-        pipeletParamValues.put( key, (String) value );
+        pipeletParamValues.put(key, (String) value);
     }
 
-    /**
-     * @return the componentName
-     */
     public String getComponentName() {
 
         return componentName;
     }
 
-    /**
-     * @param componentName the componentName to set
-     */
-    public void setComponentName( String componentName ) {
+    public void setComponentName(String componentName) {
 
         this.componentName = componentName;
     }
 
-    /**
-     * @return the name
-     */
+    @Size(min = 1, message = "{service.error.name.required}")
     public String getName() {
 
         return name;
     }
 
-    /**
-     * @param name the name to set
-     */
-    public void setName( String name ) {
+    public void setName(String name) {
 
         this.name = name;
     }
 
-    /**
-     * @return the nxServiceId
-     */
     public int getNxServiceId() {
 
         return nxServiceId;
     }
 
-    /**
-     * @param nxServiceId the nxServiceId to set
-     */
-    public void setNxServiceId( int nxServiceId ) {
+    public void setNxServiceId(int nxServiceId) {
 
         this.nxServiceId = nxServiceId;
     }
 
-    /**
-     * @return the nxComponentId
-     */
     public int getNxComponentId() {
 
         return nxComponentId;
     }
 
-    /**
-     * @param nxComponentId the nxComponentId to set
-     */
-    public void setNxComponentId( int nxComponentId ) {
+    public void setNxComponentId(int nxComponentId) {
 
         this.nxComponentId = nxComponentId;
     }
 
-    /**
-     * @return the parameters
-     */
     public List<ServiceParamPojo> getParameters() {
 
         return parameters;
     }
 
-    /**
-     * @param parameters the parameters to set
-     */
-    public void setParameters( List<ServiceParamPojo> parameters ) {
+    public void setParameters(List<ServiceParamPojo> parameters) {
 
         this.parameters = parameters;
     }
 
-    /**
-     * @return the submitted
-     */
     public String getSubmitted() {
 
         return submitted;
     }
 
-    /**
-     * @param submitted the submitted to set
-     */
-    public void setSubmitted( String submitted ) {
+    public void setSubmitted(String submitted) {
 
         this.submitted = submitted;
     }
 
-    /**
-     * @return the logFilterValues
-     */
-    public HashMap<String, String> getLogFilterValues() {
+    public Map<String, String> getLogFilterValues() {
 
-        if ( logFilterValues == null ) {
+        if (logFilterValues == null) {
             logFilterValues = new HashMap<String, String>();
         }
         return logFilterValues;
     }
 
-    /**
-     * @param logFilterValues the logFilterValues to set
-     */
-    public void setLogFilterValues( HashMap<String, String> logFilterValues ) {
+    public void setLogFilterValues(Map<String, String> logFilterValues) {
 
         this.logFilterValues = logFilterValues;
     }
 
-    /**
-     * @param key
-     * @return
-     */
-    public Object getLogFilterValue( String key ) {
+    public Object getLogFilterValue(String key) {
 
-        if ( logFilterValues == null ) {
+        if (logFilterValues == null) {
             logFilterValues = new HashMap<String, String>();
         }
-        return logFilterValues.get( key );
+        return logFilterValues.get(key);
     }
 
-    /**
-     * @param key
-     * @param value
-     */
-    public void setLogFilterValue( String key, Object value ) {
+    public void setLogFilterValue(String key, Object value) {
 
-        logFilterValues.put( key, (String) value );
+        logFilterValues.put(key, (String) value);
     }
 
-    /**
-     * @return the threshold
-     */
     public int getPosition() {
 
         return position;
     }
 
-    /**
-     * @param threshold the threshold to set
-     */
-    public void setPosition( int threshold ) {
+    public void setPosition(int position) {
 
-        this.position = threshold;
+        this.position = position;
     }
 
-    /**
-     * @return the paramsNxComponentId
-     */
     public int getParamsNxComponentId() {
 
         return paramsNxComponentId;
     }
 
-    /**
-     * @param paramsNxComponentId the paramsNxComponentId to set
-     */
-    public void setParamsNxComponentId( int paramsNxComponentId ) {
+    public void setParamsNxComponentId(int paramsNxComponentId) {
 
         this.paramsNxComponentId = paramsNxComponentId;
     }
@@ -336,27 +262,20 @@ public class ServiceForm extends ActionForm {
         return serviceInstance;
     }
 
-    public void setServiceInstance( Service serviceInstance ) {
+    public void setServiceInstance(Service serviceInstance) {
 
         this.serviceInstance = serviceInstance;
     }
 
     
-    /**
-     * @return the autostart
-     */
     public boolean isAutostart() {
     
         return autostart;
     }
 
-    
-    /**
-     * @param autostart the autostart to set
-     */
-    public void setAutostart( boolean autostart ) {
+    public void setAutostart(boolean autostart) {
     
         this.autostart = autostart;
     }
 
-} // NotifierForm
+}
