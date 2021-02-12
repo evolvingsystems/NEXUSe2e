44549f93d379b0690e15ef0426fe529d98ba05f9
Annika Borken <Annika.Borken@direkt-gruppe.de>
2021-02-12 16:38:24 +0100

    eslint configs

--- nexuse2e-webapp/src/main/frontend/.eslintrc.js
+++ nexuse2e-webapp/src/main/frontend/.eslintrc.js
@@ -5,15 +5,26 @@ module.exports = {
     jest: true,
     jasmine: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  plugins: ["@typescript-eslint"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
   rules: {
-    "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars-experimental": 2,
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    // note you must disable the base rule as it can report incorrect errors
+    "no-empty-function": "off",
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      { allow: ["private-constructors"] },
+    ],
   },
 };
