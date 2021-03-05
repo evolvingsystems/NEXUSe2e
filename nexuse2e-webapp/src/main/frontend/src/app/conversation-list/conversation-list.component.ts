dff0206f1af5e2bf3498e8f144462320ff6ac2f5
Nathalie Schulz <natha.schulz@gmail.com>
2021-03-05 12:20:14 +0100

    change css to scss files, default to scss when generating components

--- nexuse2e-webapp/src/main/frontend/src/app/conversation-list/conversation-list.component.ts
+++ nexuse2e-webapp/src/main/frontend/src/app/conversation-list/conversation-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-conversation-list",
   templateUrl: "./conversation-list.component.html",
-  styleUrls: ["./conversation-list.component.css"],
+  styleUrls: ["./conversation-list.component.scss"],
 })
 export class ConversationListComponent implements OnInit {
   constructor() {}
