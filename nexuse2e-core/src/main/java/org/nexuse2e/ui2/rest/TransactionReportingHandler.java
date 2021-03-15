9a6f938a11d7d93cbc1f4f8e8d97038afeb857ed
Annika Borken <Annika.Borken@direkt-gruppe.de>
2021-03-15 10:07:45 +0100

    10871 conversations backend service

--- nexuse2e-core/src/main/java/org/nexuse2e/ui2/rest/TransactionReportingHandler.java
+++ nexuse2e-core/src/main/java/org/nexuse2e/ui2/rest/TransactionReportingHandler.java
@@ -1,9 +1,12 @@
 package org.nexuse2e.ui2.rest;
 
 import com.google.gson.Gson;
+import org.apache.commons.lang.StringUtils;
 import org.nexuse2e.Engine;
 import org.nexuse2e.dao.TransactionDAO;
+import org.nexuse2e.pojo.ConversationPojo;
 import org.nexuse2e.pojo.MessagePojo;
+import org.nexuse2e.reporting.StatisticsConversation;
 import org.nexuse2e.reporting.StatisticsMessage;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,11 +20,26 @@ import static org.nexuse2e.util.DateUtil.getCurrentDateMinusWeeks;
 public class TransactionReportingHandler implements Handler {
     @Override
     public boolean canHandle(String path, String method) {
-        return ("GET".equalsIgnoreCase(method) && "/messages".equalsIgnoreCase(path));
+        return ("GET".equalsIgnoreCase(method) && "/messages".equalsIgnoreCase(path)) ||
+                ("GET".equalsIgnoreCase(method) && "/conversations".equalsIgnoreCase(path));
     }
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String path = request.getPathInfo();
+        if (path != null) {
+            switch (StringUtils.lowerCase(path)) {
+                case "/messages":
+                    getMessages(request, response);
+                    break;
+                case "/conversations":
+                    getConversations(request, response);
+                    break;
+            }
+        }
+    }
+
+    private void getMessages(HttpServletRequest request, HttpServletResponse response) throws Exception {
         // TODO get date range from filter
         List<MessagePojo> reportMessages = Engine.getInstance().getTransactionService()
                 .getMessagesForReport(null, 0, 0, null, null, null,
@@ -32,5 +50,18 @@ public class TransactionReportingHandler implements Handler {
         }
         String messageJson = new Gson().toJson(messages);
         response.getOutputStream().print(messageJson);
+    }
+
+    private void getConversations(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // TODO get date range from filter
+        List<ConversationPojo> reportConversations = Engine.getInstance().getTransactionService().getConversationsForReport(
+                null, 0, 0, null, getCurrentDateMinusWeeks(2), new Date(),
+                20, 0, TransactionDAO.SORT_CREATED, false);
+        List<StatisticsConversation> conversations = new LinkedList<>();
+        for (ConversationPojo conversationPojo : reportConversations) {
+            conversations.add(new StatisticsConversation(conversationPojo));
+        }
+        String conversationJson = new Gson().toJson(conversations);
+        response.getOutputStream().print(conversationJson);
     }
 }
