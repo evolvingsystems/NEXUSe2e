5faf158a7a2da3fc273998400ed7199bbd83eac1
Nathalie Schulz <natha.schulz@gmail.com>
2021-03-22 08:47:26 +0100

    add test for select filter

--- nexuse2e-webapp/src/main/frontend/src/app/select-filter/select-filter.component.spec.ts
+++ nexuse2e-webapp/src/main/frontend/src/app/select-filter/select-filter.component.spec.ts
@@ -1,16 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { SelectFilterComponent } from './select-filter.component';
+import { SelectFilterComponent } from "./select-filter.component";
 import { TranslateModule } from "@ngx-translate/core";
+import { MatSelectModule } from "@angular/material/select";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { By } from "@angular/platform-browser";
 
-describe('SelectFilterComponent', () => {
+describe("SelectFilterComponent", () => {
   let component: SelectFilterComponent;
   let fixture: ComponentFixture<SelectFilterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SelectFilterComponent],
-      imports: [TranslateModule.forRoot()]
+      imports: [TranslateModule.forRoot(), MatSelectModule, NoopAnimationsModule]
     })
       .compileComponents();
   });
@@ -18,10 +21,35 @@ describe('SelectFilterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectFilterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should display an option for each allowed value", async () => {
+    component.allowedValues = ["One", "Two", "Three"];
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement;
+    const matSelect = debugElement.query(By.css('.mat-select-trigger')).nativeElement;
+    matSelect.click();
+    fixture.detectChanges();
+    const matOptions = debugElement.queryAll(By.css('.mat-option'));
+
+    expect(matOptions.length).toBe(component.allowedValues.length + 1);
+  });
+
+  it("should emit the value when something is selected", () => {
+    spyOn(component, "emitValue");
+    component.allowedValues = ["Three", "Four", "Five"];
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement;
+    const matSelect = debugElement.query(By.css('.mat-select-trigger')).nativeElement;
+    matSelect.click();
+    fixture.detectChanges();
+    const matOptions = debugElement.queryAll(By.css('.mat-option'));
+    matOptions[1].nativeElement.click();
+
+    expect(component.emitValue).toHaveBeenCalled();
+  })
 });
