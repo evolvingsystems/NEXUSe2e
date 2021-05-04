b0c6a324c0dd984fa093a48347cce244e5624584
Nathalie Schulz <natha.schulz@gmail.com>
2021-05-04 09:39:06 +0200

    add tests to list component

--- nexuse2e-webapp/src/main/frontend/src/app/list/list.component.spec.ts
+++ nexuse2e-webapp/src/main/frontend/src/app/list/list.component.spec.ts
@@ -5,8 +5,10 @@ import { MatTableModule } from "@angular/material/table";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TranslateModule } from "@ngx-translate/core";
+import { messages } from "../test-data";
+import { MatCardModule } from "@angular/material/card";
 
-describe("TableComponent", () => {
+describe("ListComponent", () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
 
@@ -15,6 +17,7 @@ describe("TableComponent", () => {
       imports: [
         MatTableModule,
         MatCheckboxModule,
+        MatCardModule,
         RouterTestingModule,
         TranslateModule.forRoot(),
       ],
@@ -25,6 +28,41 @@ describe("TableComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
+    component.mobileConfig = [
+      {
+        fieldName: "messageId",
+        linkUrlRecipe: "$nxMessageId$",
+        isHeader: true,
+      },
+      {
+        fieldName: "conversationId",
+        linkUrlRecipe: "../conversations/$nxConversationId$",
+      },
+      { fieldName: "partnerId" },
+      { fieldName: "typeName" },
+      { fieldName: "actionId" },
+      { fieldName: "createdDate" },
+    ];
+    component.desktopConfig = [
+      {
+        fieldName: "messageId",
+        linkUrlRecipe: "$nxMessageId$",
+      },
+      {
+        fieldName: "conversationId",
+        linkUrlRecipe: "../conversations/$nxConversationId$",
+      },
+      { fieldName: "partnerId" },
+      { fieldName: "status" },
+      { fieldName: "backendStatus" },
+      { fieldName: "typeName" },
+      { fieldName: "choreographyId" },
+      { fieldName: "actionId" },
+      { fieldName: "createdDate" },
+      { fieldName: "turnAroundTime" },
+    ];
+    component.items = messages;
+    component.itemType = "message";
     fixture.detectChanges();
   });
 
@@ -32,14 +70,68 @@ describe("TableComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it("should find the header element if it is configured");
-  it("should get property by propertyName");
-  it("should thrown an error if propertyName does not exist");
-  it("should show a checkbox if it is configured");
-  it("should build the item url if configured");
-  // $nxConversationId$
+  it("should show a header element if it is configured in mobile with a linkUrlRecipe", () => {
+    spyOn(component.screenSizeService, "isMobile").and.returnValue(true);
+    component.mobileConfig = [{
+      fieldName: "messageId",
+      linkUrlRecipe: "$nxMessageId$",
+      isHeader: true,
+    }];
+    component.items = messages;
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector(".card-header")).toBeTruthy();
+  });
+
+  it("should get property by propertyName", () => {
+    const message = {
+      messageId: "640FE4F8-6A6D-B7D8-E35B-FB7FE4764426",
+      actionId: "SendFile",
+      choreographyId: "GenericFile",
+      createdDate: "01-12-2022 15:21:03 GMT",
+      typeName: "Normal",
+      status: "Sent",
+      conversationId: "CEBC3B83-3648-41C6-F033-E8B8A4A48AF8",
+      nxMessageId: 1,
+      nxConversationId: 1,
+      partnerId: "Victor",
+      backendStatus: "QUEUED",
+      turnAroundTime: "Not terminated",
+    };
+    const propertyValue = component.getProperty(message, "nxConversationId");
+    expect(propertyValue).toEqual(message.nxConversationId);
+  });
+
+  it("should display one column for each configured ListConfig item in desktop", () => {
+    spyOn(component.screenSizeService, "isMobile").and.returnValue(false);
+    fixture.detectChanges();
+    const columns = fixture.nativeElement.querySelectorAll("th");
+    // display columns contain desktopConfig + select column (even if isSelectable is false)
+    expect(columns.length).toEqual(component.desktopConfig.length + 1);
+  });
+
+  it("should display one card row for each configured ListConfig item in mobile", () => {
+    spyOn(component.screenSizeService, "isMobile").and.returnValue(true);
+    fixture.detectChanges();
+    const firstCard = fixture.nativeElement.querySelector("mat-card");
+    const header = firstCard.querySelectorAll(".card-header");
+    const rows = firstCard.querySelectorAll("tr");
+    expect(rows.length).toEqual(component.mobileConfig.length - header.length);
+  });
+
+  it("should build the item url if it consists of one variable", () => {
+    const item = messages[0];
+    const recipe = "$nxConversationId$";
+    const builtUrl = component.getUrl(item, recipe);
+    expect(builtUrl).toEqual("" + item.nxConversationId);
+  });
+
   // /base/test
+  it("should build the item url if it contains no variables");
+
   // /base/test/$nxConversationId$/test2/$nxConversationId$/
+  it("should build the item url if it contains multiple variables in the middle");
+
   // $nxConversationId$/test/test3/$nxConversationId$
-  // ""
+  it("should build the item url if it contains multiple variables on the edges");
 });
