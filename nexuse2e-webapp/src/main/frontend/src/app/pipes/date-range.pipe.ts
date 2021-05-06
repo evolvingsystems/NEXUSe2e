d3c2fdfd6bcae55634b47f8bcf62216e8493685a
Nathalie Schulz <natha.schulz@gmail.com>
2021-05-06 07:41:29 +0200

    Implement suggested changes from PR

--- nexuse2e-webapp/src/main/frontend/src/app/pipes/date-range.pipe.ts
+++ nexuse2e-webapp/src/main/frontend/src/app/pipes/date-range.pipe.ts
@@ -7,6 +7,9 @@ import { DateRange } from "../types";
 })
 export class DateRangePipe implements PipeTransform {
   transform(value: unknown): DateRange {
+    if (value && (value as DateRange).startDate == undefined) {
+      console.error("Object", value, "is not of type DateRange.");
+    }
     return value as DateRange;
   }
 }
