8550b8b0d17130f5dd81a1bae6f5e6d00dd63afa
Nathalie Schulz <natha.schulz@gmail.com>
2021-05-10 11:45:16 +0200

    11505 update permissions on site reload

--- nexuse2e-webapp/src/main/frontend/src/app/services/permission.service.ts
+++ nexuse2e-webapp/src/main/frontend/src/app/services/permission.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { SessionService } from "../services/session.service";
+import { DataService } from "./data.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermissionService {
 
-  constructor(private sessionService: SessionService) {
+  constructor(private sessionService: SessionService, private dataService: DataService) {
+  }
+
+  async updatePermissions() {
+    const permittedActions = await this.dataService.getPermittedActions();
+    this.sessionService.setPermittedActions(permittedActions);
   }
 
   isUserPermitted(actionKey: string) {
